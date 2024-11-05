9f739a6d707e16b3d04f2394194492ce6e5bc90a
Veronika Mezentseva <nika.me18@yandex.ru>
2024-11-05 17:17:05 +0300

    next config modified

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/4A-Consulting/' : '',
-  basePath: isProd ? '/4A-Consulting' : '',
+  assetPrefix: '/4A-Consulting/',
+  basePath: '/4A-Consulting',
   output: 'export'
 };
 
