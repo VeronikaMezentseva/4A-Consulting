2f60f6060cbd8cc5503f5cd1e7eeadfa40d2df2e
Veronika Mezentseva <nika.me18@yandex.ru>
2024-11-05 17:55:40 +0300

    Updates

--- _next/static/VMLMcZmEdyxnWOiG1owQW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/419-a17ed84fc66dfb5f.js","static/chunks/pages/index-872d274b1ce34f99.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
